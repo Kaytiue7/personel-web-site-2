b5aa18ca8be7d9a7d403502c54313d0594eeb9c5
Kaytiue7 <Kaytiue7@users.noreply.github.com>
2025-04-07 11:57:32 +0300

    kys

--- src/app/components/portfolio.tsx
+++ src/app/components/portfolio.tsx
@@ -5,13 +5,25 @@ import { FaGithub } from "react-icons/fa";
 
 export default function Portfolio () {
     return (
-        <div className="text-black w-full mx-auto flex flex-col justify-between md:px-8 items-center gap-8 md:h-[90lvh] p-4">
+        <div className="text-black w-full mx-auto flex flex-col justify-between md:px-8 items-center gap-8 md:min-h-[90lvh] p-4">
             <div>
-                <h1 className="text-5xl md:text-8xl font-bold py-6">Portfolio</h1>
+                <h1 className="text-5xl md:text-8xl font-bold py-2">Portfolio</h1>
             </div>
 
-            <div className="flex w-full h-full p-4 justify-center">
-                <div className="flex flex-col-reverse md:w-auto md:h-full h-auto rounded-lg p-8 md:p-4 border border-black shadow-xl shadow-black gap-6">
+            <div className="flex justify-center">
+                <div className="flex flex-col rounded-lg border border-black shadow-xl shadow-black p-4 gap-6">
+                    <div className="flex justify-between w-full h-full">
+                        <div className="md:px-3">
+                            <FaGithub className="text-black text-6xl cursor-pointer opacity-0 absolute md:opacity-100 md:relative" />
+                        </div>
+                        
+                        <img src='/dex.jpg' className="flex self-center justify-center rounded-4xl shadow-lg shadow-black"></img>
+                        
+                        <div className="md:px-3">
+                            <FaGithub className="text-black text-6xl cursor-pointer opacity-0 absolute md:relative" />
+                        </div>
+                    </div>
+
                     <div className="flex flex-row justify-between items-center md:w-full md:h-full">
                         <div>
                           <button 
@@ -37,18 +49,6 @@ export default function Portfolio () {
 
                         
                         
-                    </div>
-
-                    <div className="flex justify-between w-full h-full">
-                        <div className="md:px-3">
-                            <FaGithub className="text-black text-6xl cursor-pointer opacity-0 absolute md:opacity-100 md:relative" />
-                        </div>
-                        
-                        <img src='/dex.jpg' className="flex aspect-square self-center justify-center rounded-4xl shadow-lg shadow-black"></img>
-                        
-                        <div className="md:px-3">
-                            <FaGithub className="text-black text-6xl cursor-pointer opacity-0 absolute md:relative" />
-                        </div>
                     </div>
                     
                 </div>
